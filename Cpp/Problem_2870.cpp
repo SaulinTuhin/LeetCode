f0ccdbf3d9cfdaa3311b5ee2cb6da15ff270ad40
Saulin Tuhin <saulintuhin@gmail.com>
2024-01-05 00:28:09 +0600

    Completed 2870
    Signed-off-by: Saulin Tuhin <saulintuhin@gmail.com>

--- Cpp/Problem_2870.cpp
+++ Cpp/Problem_2870.cpp
@@ -1,48 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <cmath>
 
 using namespace std;
 
+// Problem - 2870: https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/
+// C++ Solution!
 class Solution
 {
 public:
-    int minOperations(vector<int> &nums)
-    {
-        unordered_map<int, int> counts;
+	int minOperations(vector<int> &nums)
+	{
+		unordered_map<int, int> counts;
 
-        for (int num : nums)
-        {
-            counts[num]++;
-        }
+		for (int num : nums)
+		{
+			counts[num]++;
+		}
 
-        int res = 0;
-        for (auto count : counts)
-        {
-            if (count.second % 3 == 0)
-            {
-                res += count.second / 3;
-                count.second %= 3;
-            }
-            if (count.second % 2 == 0)
-            {
-                res += count.second / 2;
-                count.second %= 2;
-            }
-        }
+		int res = 0;
+		for (auto count : counts)
+		{
+			if (count.second == 1)
+				return -1;
 
-        return res == 0 ? -1 : res;
-    }
+			res += ceil((double)count.second / 3);
+		}
+
+		return res;
+	}
 };
 
 int main()
 {
-    Solution sol;
+	Solution sol;
 
-    vector<int> input = {2, 3, 3, 2, 2, 4, 2, 3, 4};
-    cout << sol.minOperations(input) << endl;
-    input = {2, 1, 2, 2, 3, 3};
-    cout << sol.minOperations(input) << endl;
+	vector<int> input = { 2, 3, 3, 2, 2, 4, 2, 3, 4 };
+	cout << sol.minOperations(input) << endl;
+	input = { 2, 1, 2, 2, 3, 3 };
+	cout << sol.minOperations(input) << endl;
 
-    return 0;
+	return 0;
 }
