79f9f68a2590d02bab15362a0ccef50c78dfb1eb
Saulin Tuhin <saulintuhin@gmail.com>
2025-01-17 15:27:43 +0000

    Adding solution for LeetCode 2683
    Signed-off-by: Saulin Tuhin <saulintuhin@gmail.com>

---
+++ Python/Problem_2683.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        return sum(derived) % 2 == 0
+
+    def doesValidArrayExist_1(self, derived: List[int]) -> bool:
+        res = 0
+        for n in derived:
+            res ^= n
+        return not res
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    print(sol.doesValidArrayExist([1, 1, 0]))
+    print(sol.doesValidArrayExist([1, 1]))
+    print(sol.doesValidArrayExist([1, 0]))
